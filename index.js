ae1085a96a628d603e3ff76c0af2308b52c4b063
rakib-sikder <itsmebroiam@gmail.com>
2025-06-10 23:56:44 +0600

    feat: integrate socket.io for real-time communication

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express=require('express');
 const cors=require('cors');
 const mongoose=require('mongoose');
 require('dotenv').config();
-const app=express();
+const http = require('http');
+const {Server}= require('socket.io');
+const bcrypt = require('bcryptjs');
+const jwt =require('jsonwebtoken');
+const PORT= process.env.PORT || 5000;
 
-const PORT= 5000;
+
+const app=express();
+const server = http.createServer(app)
+
 app.use(cors());
 app.use(express.json());
 
@@ -15,6 +22,11 @@ mongoose
   .catch((err) => console.error('MongoDB connection error:', err));
 
 
-  app.listen(PORT, () => {
+
+
+
+
+
+  server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
